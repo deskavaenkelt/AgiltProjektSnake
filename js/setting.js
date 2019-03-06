c72799df1a286a98290d1c44668c2cc1fca06443
Joshkleinv <Joshkv99@gmail.com>
2019-03-06 11:26:33 +0100

    ny snake game

---
+++ js/setting.js
@@ -0,0 +1,10 @@
+
+var mycanvas = document.getElementById('mycanvas');
+var ctx = mycanvas.getContext('2d');
+var snakeSize = 10; 
+var w = 1500;
+var h = 1500;
+var score = 0;
+var snake;
+var snakeSize = 10;
+var food;
