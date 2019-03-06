d082ad042fed5e1713222681c76bcfe064668db6
Joshkleinv <Joshkv99@gmail.com>
2019-03-06 11:28:51 +0100

    settings push

--- js/setting.js
+++ js/setting.js
@@ -2,8 +2,8 @@
 var mycanvas = document.getElementById('mycanvas');
 var ctx = mycanvas.getContext('2d');
 var snakeSize = 10; 
-var w = 1500;
-var h = 1500;
+var w = 350;
+var h = 350;
 var score = 0;
 var snake;
 var snakeSize = 10;
