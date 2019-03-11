3ff41be38bbf4245c3b6be387dd4f705a5d390b9
Joshkleinv <Joshkv99@gmail.com>
2019-03-11 13:24:57 +0100

    testat saker med canvas

--- js/app.js
+++ js/app.js
@@ -1,8 +1,31 @@
 (function (window, document, drawModule) {
 
 var btnStart = document.getElementById('btnStart');
-btnStart.addEventListener("click", function(){ drawModule.init();});
+    btnStart.addEventListener("click", function(){ drawModule.init();});
+//btnStart.addEventListener("click", function(){ drawModule.init();});
+// Button position and dimensions
+ /*   var buttonX = 300;
+    var buttonY = 375;
+    var buttonW = 60;
+    var buttonH = 30;
 
+// Render button
+    ctx.fillStyle = 'red';
+    ctx.fillRect(buttonX, buttonY, buttonW, buttonH);
+
+    // Control that click event occurred within position of button
+    // NOTE: This assumes canvas is positioned at top left corner
+    if (
+        event.x > buttonX &&
+        event.x < buttonX + buttonW &&
+        event.y > buttonY &&
+        event.y < buttonY + buttonH
+    ) {
+        // Executes if button was clicked!
+
+
+    }
+    */
 
 	document.onkeydown = function(event) {
 
--- js/setting.js
+++ js/setting.js
@@ -2,6 +2,10 @@ var mycanvas = document.getElementById('mycanvas');
 var ctx = mycanvas.getContext('2d');
 var snakeSize = 15;
 
+//ctx.button = '50px Courier New'
+//var startstart = ctx.button('Start', 300, 375);
+
+
 var w = 750;
 var h = 750;
 var score = 0;
