630960310a411638f4adb01f7f8bbf3dfa70ea5d
Lars Strömberg <stromberg.lars@gmail.com>
2019-03-18 19:49:56 +0100

    Update setting.js
    Cleanup code and modified for Hard Mode

--- js/setting.js
+++ js/setting.js
@@ -1,11 +1,6 @@
 let mycanvas = document.getElementById('mycanvas');
 let ctx = mycanvas.getContext('2d');
 let snakeSize = 15;
-
-//ctx.button = '50px Courier New'
-//let startstart = ctx.button('Start', 300, 375);
-
-
 let w = 750;
 let h = 750;
 let score = 0;
@@ -14,3 +9,27 @@ let highScoreList= [];
 let localStorageName = "storage";
 let snake;
 let food;
+
+
+// Hasse property's
+let snakeLength;
+let snakeSpeed;
+
+
+/*
+* Hard
+*/
+let hardMode = false;
+let power;
+
+// Power up/down
+let booleanPowerUpLength;
+let booleanPowerDownLength;
+let booleanPowerUpSpeed;
+let booleanPowerDownSpeed;
+
+
+// Legacy ?
+
+//ctx.button = '50px Courier New'
+//let startstart = ctx.button('Start', 300, 375);
