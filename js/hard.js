56859367647ee2e7c7b9637f876b5fad17694da0
Lars Strömberg <stromberg.lars@gmail.com>
2019-03-18 19:49:44 +0100

    Update hard.js

--- js/hard.js
+++ js/hard.js
@@ -1,58 +1,9 @@
 // 50x50 rutor
-let power;
-let powerSomethingGenerator = (function () {
 
-
-    let _privateMethod = function () {
-        // private stuff
-    };
-
-    let publicMethod = function () {
-        _privateMethod();
-    };
-
-    return {
-        publicMethod: publicMethod
-    };
-
-})();
-powerSomethingGenerator.publicMethod();
-
-
-
-let powerUpLength = (function () {
-    let propertyPosition = generatePowerPosition.publicMethod();
-
-    s
-
-
-
-
-    let _privateMethod = function () {
-        snakeLength = snakeLength -2;
-    };
-
-    let publicMethod = function () {
-        _privateMethod();
-    };
-
-    return {
-        publicMethod: publicMethod
-    };
-
-})();
-
-
-
-
-
-
-
-
-
-
-
-
+function generateAPower() {
+    generatePowerPosition();
+    randomPower();
+}
 
 let generatePowerPosition = (function() {
     let _privateMethod = function () {
@@ -61,11 +12,14 @@ let generatePowerPosition = (function() {
             y: Math.floor((Math.random() * 30) + 1)
         };
 
-        for (let i=0; i>snake.length; i++) {
+        for (let i = 0; i > snake.length; i++) {
             let snakeX = snake[i].x;
             let snakeY = snake[i].y;
 
-            if (power.x === snakeX && power.y === snakeY || power.y === snakeY && power.x === snakeX) {
+            // Don't generat on food or snake
+            if (power.x === snakeX && power.y === snakeY || power.y === snakeY && power.x === snakeX ||
+                power.x === food.x && power.y === food.y || power.y === food.y && power.x === food.x)
+            {
                 power.x = Math.floor((Math.random() * 30) + 1);
                 power.y = Math.floor((Math.random() * 30) + 1);
             }
@@ -80,3 +34,118 @@ let generatePowerPosition = (function() {
         publicMethod: publicMethod
     };
 })();
+
+let randomPower = (function () {
+    let _privateMethod = function () {
+        let number = ((Math.random() * 4) + 1);
+
+        // Set everyting to false
+        booleanPowerUpLength = false;
+        booleanPowerDownLength = false;
+        booleanPowerUpSpeed = false;
+        booleanPowerDownSpeed = false;
+
+        // Set only one power to True based on generated number
+        if (number === 1) {
+            booleanPowerUpLength = true;
+        } else if (number === 2) {
+            booleanPowerDownLength = true;
+        } else if (number === 3) {
+            booleanPowerUpSpeed = true;
+        } else {
+            booleanPowerDownSpeed = true;
+        }
+    };
+
+    let publicMethod = function () {
+        _privateMethod();
+    };
+
+    return {
+        publicMethod: publicMethod
+    };
+})();
+
+
+let activePower = (function () {
+    let _privateMethod = function () {
+        if (booleanPowerUpLength) {
+            if (7 <= snakeLength) {
+                snakeLength -= 4;
+            }
+        } else if (booleanPowerDownLength) {
+            snakeLength += 4;
+        } else if (booleanPowerUpSpeed) {
+
+        } else if (booleanPowerDownSpeed) {
+
+        }
+    };
+
+    let publicMethod = function () {
+        _privateMethod();
+    };
+
+    return {
+        publicMethod: publicMethod
+    };
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// work in progress below
+
+
+let powerUpLength = (function () {
+    //let propertyPosition = generatePowerPosition.publicMethod();
+
+    let _privateMethod = function () {
+        if (7 <= snakeLength) {
+            snakeLength -= 4;
+        }
+    };
+
+    let publicMethod = function () {
+        _privateMethod();
+    };
+
+    return {
+        publicMethod: publicMethod
+    };
+
+})();
+let powerSomethingGenerator = (function () {
+
+
+    let _privateMethod = function () {
+        // private stuff
+
+        powerUpLength.publicMethod();
+    };
+
+    let publicMethod = function () {
+        _privateMethod();
+    };
+
+    return {
+        publicMethod: publicMethod
+    };
+
+})();
+powerSomethingGenerator.publicMethod();
+
+
