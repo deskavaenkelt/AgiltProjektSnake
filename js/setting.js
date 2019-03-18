0c568333d27397b205a0180ed8105bf634eed424
Lars Strömberg <stromberg.lars@gmail.com>
2019-03-18 20:40:36 +0100

    Update setting.js
    Reorderd and added support for Themes

--- js/setting.js
+++ js/setting.js
@@ -1,12 +1,30 @@
+// Canvas
 let mycanvas = document.getElementById('mycanvas');
 let ctx = mycanvas.getContext('2d');
 let snakeSize = 15;
 let w = 750;
 let h = 750;
+
+// Theme
+let themeClassic = true;
+let themeDjungel;
+let themeOken;
+let themeAkvarie;
+// Theme buttons
+let btnClassic = document.getElementById('btnClassic');
+let btnDjungel = document.getElementById('btnDjungel');
+let btnOken = document.getElementById('btnOken');
+let btnAkvarie = document.getElementById('btnAkvarie');
+
+
+// HighScore
 let score = 0;
 let highScore;
 let highScoreList= [];
 let localStorageName = "storage";
+
+
+// Gameplay
 let snake;
 let food;
 
