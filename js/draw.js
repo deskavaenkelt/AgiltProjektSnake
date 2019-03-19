01bfac24d8f07a59d5670eac1e7f7c7598a5b42d
Mattan1999 <mattias470@gmail.com>
2019-03-19 09:04:25 +0100

    Tog bort skräpkod

--- js/draw.js
+++ js/draw.js
@@ -115,18 +115,6 @@ let drawModule = (function () {
         pizza(food.x, food.y);
         scoreText();
 
-        //If the snake eats power it makes somthing happen and this means that, in this case, you will receive powerUp or powerDown a.
-        if (snakeX === power.x && snakeY === power.y) {
-            // Activate powerUp/Down
-
-            // Create new power.
-            createFood();
-        } else {
-
-            // Check if 10 sek has passed since last generated power, remove and generate new if true
-
-        }
-
     };
 
 
